8837cff4611c2df1c9c7570252ec0d5bb02b5c94
HanryKrobel <168023395+HanryLedoux@users.noreply.github.com>
2026-06-10 19:27:31 -0300

    Arrumando background de campo

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -11,8 +11,8 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-input-label for="email" :value="__('Email')" style="color:#d1d5db" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus style="background:#fff;border-color:rgba(255,255,255,0.2);color:#111" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
